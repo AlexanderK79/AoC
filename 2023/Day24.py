28b6bdd350f601609ecbf886e20e6b37d06cdf15
Alexander Kraijenoord <a.c.kraijenoord@lumc.nl>
2024-01-22 21:36:26 +0100

    Day24 works for the test set, but not for P to Reddit we go

--- 2023/Day24.py
+++ 2023/Day24.py
@@ -1,5 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
+from collections import Counter
 
 class hailStorm:
     def __init__(self) -> None:
@@ -7,12 +8,14 @@ class hailStorm:
         self.hailstones = list()
         self.intersections = dict()
         self.minXY, self.maxXY = int(), int()
-
+        self.sims = {}
+        self.hailstones_p2 =[]
     def build(self, fContent):
         self.content = fContent
         for thisLine in fContent:
             (x,y,z),(dx,dy,dz) = [map(int, p.split(', ')) for p in thisLine.split(' @ ')]
             self.hailstones.append(line_2D(dy/dx, (y+(dy*(x/-dx))), fFuture=(y, -1 if dy < 0 else 1)))
+            self.hailstones_p2.append(line_3D(x,y,z, dx,dy,dz))
         pass
         # with an X and Y position each at least 7 and at most 27, production 200000000000000, 400000000000000;
         if args.production:
@@ -29,6 +32,14 @@ class line_2D:
         self.future = fFuture # init y value and value of y shrinks (-1) or grows (+1) in the future
         pass
 
+class line_3D:
+    def __init__(self, fX, fY, fZ, fdX, fdY, fdZ) -> None:
+        self.name = ','.join(map(str, (fX,fY,fZ))) + '_' + ','.join(map(str, (fdX,fdY,fdZ)))
+        self.x, self.y, self.z = fX, fY, fZ
+        self.dx, self.dy, self.dz = fdX, fdY, fdZ
+    def calc(self, t):
+        return (self.x + (t*self.dx), self.y + (t*self.dy), self.z + (t*self.dz))
+        
 def line_intersect(L1, L2, fMin, fMax):
     if (L2.slope-L1.slope) == 0: # parallel lines
         return (0, (False, False))
@@ -66,9 +77,37 @@ def main(stdscr):
 
     print(20 * '*')
 
-    result = result
+    repfound = False
+    t = -1
+    while not repfound:
+        t += 1
+        if t % 50 == 0: print('processing t', t)
+        # assert t<50, 't should be smaller than 50'
+        myHS.sims[t] = {i.name: {'pos': i.calc(t), 'dist': dict()} for i in myHS.hailstones_p2[:3]}
+        if t == 0: continue
+        # make a diff to each prev point
+        for thisName,thisPos in myHS.sims[t].items():
+            for prevT in range(1, t):
+                for prevName, prevPos in myHS.sims[prevT].items():
+                    if prevName == thisName: continue
+                    if prevT == 1: myHS.sims[t][thisName]['dist'][prevName] = {}
+                    myHS.sims[t][thisName]['dist'][prevName][prevT] = tuple([(a-b)/(t-prevT) for a,b in zip(thisPos['pos'], prevPos['pos'])])
+                    pass
+        # get all diffs
+        all_diffs = [item for sublist in [[item for sublist in [list(p['dist'].values()) for p in vals.values()] for item in sublist] for t,vals in myHS.sims.items() if t>1] for item in sublist]
+        all_diffs = [item for sublist in [list(i.values()) for i in all_diffs] for item in sublist]
+        # get most occurences
+        best_guess = [(d,c) for d,c in Counter(all_diffs).items() if c>2]
+        if len(best_guess) > 0:
+            repfound = True
+    best_guess = sorted(best_guess, key=lambda item:(item[1], item[0]), reverse=True )[0]
+    pass
 
-    message = f'The answer to part 2 is (sample should be 2, answer should be x): {result}'
+
+
+    result = best_guess
+
+    message = f'The answer to part 2 is (sample should be 47, answer should be x): {result}'
     print(message)
     pass
 
