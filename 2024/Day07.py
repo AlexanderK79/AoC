9ab099cad4eefc2bf4fad2af1b96617ec32b1447
Alexander Cornelis Kraijenoord <A.C.Kraijenoord@lumc.nl>
2024-12-08 07:54:55 +0100

    Day 07 with recursion and fast!

--- 2024/Day07.py
+++ 2024/Day07.py
@@ -1,5 +1,12 @@
 import argparse
-import itertools as it
+
+def calc(fDesResult: int, fStart: int, fNumbers: tuple, fOpList: list) -> bool:
+    if len(fNumbers) == 1: # at the end
+        return fDesResult == fStart * fNumbers[0] or fDesResult == fStart + fNumbers[0]
+    else:
+        return ( calc(fDesResult=fDesResult, fStart=fStart * fNumbers[0], fNumbers=fNumbers[1:], fOpList=fOpList+'*') or
+        calc(fDesResult=fDesResult, fStart=fStart + fNumbers[0], fNumbers=fNumbers[1:], fOpList=fOpList+'+') )
+    pass
 
 def main(stdscr):
     with open(fName, 'r+') as f:
@@ -10,46 +17,17 @@ def main(stdscr):
         r, i = line.split(': ')
         r = int(r)
         i = tuple(map(int, i.split(' ')))
-        myCal.append([r, i, [x for x in it.product(('*','+'), repeat=len(i)-1)], list(), dict()])
+        myCal.append([r, i, None])
     del r, i, line, f, stdscr, fContent
 
     for cal in myCal:
         print('processing cal', cal[:2])
-        cal[4] = dict() # dictionary of previous calculations
-        for oplist in cal[2]:
-            # check if part of this calc is already done
-            for j in range(len(oplist)+1):
-                r = cal[4].get(oplist[:-j])
-                if r is not None:
-                    # calculation already done, continue from there
-                    # print ('already know', oplist[:-j], '=', r)
-                    pass
-                else:
-                    # calculation does not exist... do it from the start
-                    r = cal[1][0]
-                    assert(j == 0, 'this should be 0 now')
-                    pass
-            
-            while r < cal[0] and r > 0:
-                for i,n in enumerate(cal[1][1:]):
-                    match oplist[i]:
-                        case '+': r += n
-                        case '*': r *= n
-                    cal[4][oplist[:i+1]] = r
-            # we broke out, now check the result
-            if r == cal[0] and i+1 == len(cal[1][1:]):
-                cal[3].append(True)
-                break
-            else:
-                cal[3].append(False)
-                # remove all entries from oplist that have the same start
-                [cal[2].remove(o) for o in cal[2] if o[:i+1] == oplist[:i+1] and o != oplist]
-                pass
+        cal[2] = calc(fDesResult=cal[0], fStart=cal[1][0], fNumbers=tuple(cal[1][1:]), fOpList='')
     pass
-    del cal, i, n, r, oplist
+    del cal
 
 
-    result = sum([r[0] for r in myCal if True in r[3]])
+    result = sum([r[0] for r in myCal if r[2]])
     message = f'The answer to part 1 is (sample should be 3749, answer should be ?): {result}\n'
     print(message)
     pass
