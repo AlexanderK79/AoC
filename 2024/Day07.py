7295a68fc3ae0bdea8a61ccf0c2fe9b45bc383a0
Alexander Cornelis Kraijenoord <A.C.Kraijenoord@lumc.nl>
2024-12-08 07:11:33 +0100

    day07... works with sample, not with input

--- 2024/Day07.py
+++ 2024/Day07.py
@@ -1,114 +1,56 @@
 import argparse
-import re
-
-class AocMap:
-    def __init__(self) -> None:
-        self.content = list()
-        self.map = list()
-        self.dirs = tuple()
-        self.height, self.width = 0,0
-        self.pos = list()
-        self.path = list()
-        pass
-    def build(self, fContent, fDirs):
-        self.content = fContent
-        self.dirs = fDirs
-        for y, line in enumerate(reversed(fContent)):
-            for x, value in enumerate(line):
-                if value == '#':
-                    self.map.append((x,y))
-                elif value in self.dirs:
-                    self.pos = [(x, y), value]
-                else:
-                    pass
-        self.path.append(((self.pos[0]), self.pos[1]))
-        self.width, self.height = x, y
-        return self
-    def move(self):
-        # return new position and direction and if it has hit the edge
-        hitEdge = False
-        if self.pos[1] in ('^', 'v'):
-            # move vertically
-            thisX = self.pos[0][0]
-            thisY = [-1] + [co[1] for co in self.map if co[0] == self.pos[0][0]] + [self.height+1]
-            if self.pos[1] == '^':
-                thisY = min([y for y in thisY if y > self.pos[0][1]])
-                hitEdge, thisY = (True, thisY) if thisY == self.height+1 else (False, thisY - 1)
-            elif self.pos[1] == 'v':
-                thisY = max([y for y in thisY if y < self.pos[0][1]])
-                hitEdge, thisY = (True, thisY) if thisY == -1 else (False, thisY + 1)
-            else:
-                quit()
-        elif self.pos[1] in ('>', '<'):
-            # move horizontally
-            thisX = [-1] + [co[0] for co in self.map if co[1] == self.pos[0][1]] + [self.width+1]
-            thisY = self.pos[0][1]
-            if self.pos[1] == '>':
-                thisX = min([x for x in thisX if x > self.pos[0][0]])
-                hitEdge, thisX = (True, thisX) if thisX == self.width+1 else (False, thisX - 1)
-            elif self.pos[1] == '<':
-                thisX = max([x for x in thisX if x < self.pos[0][0]])
-                hitEdge, thisX = (True, thisX) if thisX == -1 else (False, thisX + 1)
-            else:
-                quit()
-        else:
-            quit('why do get here')
-        if not hitEdge:
-            # turn right
-            self.pos[1] = self.dirs[(self.dirs.index(self.pos[1])+1) % 4]
-        self.pos[0] = (thisX, thisY)
-        return hitEdge
-    def distinctPositions(self):
-        # returns a set of distinct positions
-        visitedCos = list()
-        for i, co in enumerate(self.path[:-1]):
-            startX, endX, startY, endY =  self.path[i][0][0], self.path[i+1][0][0], self.path[i][0][1], self.path[i+1][0][1]
-            if startX == endX:
-                for j in range(min(startY, endY), max(startY, endY)+1):
-                    visitedCos.append((startX, j))
-            elif startY == endY:
-                for j in range(min(startX, endX), max(startX, endX)+1):
-                    visitedCos.append((j, startY))
-        return set(visitedCos)
-
+import itertools as it
 
 def main(stdscr):
     with open(fName, 'r+') as f:
         fContent = f.read().splitlines()
 
-    myMap = AocMap().build(fContent=fContent, fDirs = ('^', '>', 'v', '<'))
+    myCal = list()
+    for line in fContent:
+        r, i = line.split(': ')
+        r = int(r)
+        i = tuple(map(int, i.split(' ')))
+        myCal.append([r, i, [x for x in it.product(('*','+'), repeat=len(i)-1)], list(), dict()])
+    del r, i, line, f, stdscr, fContent
 
-    # find next obstacle until we hit the edge of the map
-    hitEdge = False
-    while not hitEdge:
-        hitEdge = myMap.move()
-        myMap.path.append(tuple(myMap.pos))
-    
-    # find the distinct positions
-    result = len(myMap.distinctPositions()) - 1 # remove the point that's over the edge
+    for cal in myCal:
+        print('processing cal', cal[:2])
+        cal[4] = dict() # dictionary of previous calculations
+        for oplist in cal[2]:
+            # check if part of this calc is already done
+            for j in range(len(oplist)+1):
+                r = cal[4].get(oplist[:-j])
+                if r is not None:
+                    # calculation already done, continue from there
+                    # print ('already know', oplist[:-j], '=', r)
+                    pass
+                else:
+                    # calculation does not exist... do it from the start
+                    r = cal[1][0]
+                    assert(j == 0, 'this should be 0 now')
+                    pass
+            
+            while r < cal[0] and r > 0:
+                for i,n in enumerate(cal[1][1:]):
+                    match oplist[i]:
+                        case '+': r += n
+                        case '*': r *= n
+                    cal[4][oplist[:i+1]] = r
+            # we broke out, now check the result
+            if r == cal[0] and i+1 == len(cal[1][1:]):
+                cal[3].append(True)
+                break
+            else:
+                cal[3].append(False)
+                # remove all entries from oplist that have the same start
+                [cal[2].remove(o) for o in cal[2] if o[:i+1] == oplist[:i+1] and o != oplist]
+                pass
+    pass
+    del cal, i, n, r, oplist
 
-    message = f'The answer to part 1 is (sample should be 41, answer should be 4890): {result}\n'
-    print(message)
 
-    # create a copy of the map and place an object in one of the visited points
-    # if we have been there already in the same direction, we found a loop
-    result = 0
-    
-    for i, c in enumerate([c for c in myMap.distinctPositions() if c != myMap.path[0][0]]):
-        myMap_2 = AocMap().build(fContent=fContent, fDirs = ('^', '>', 'v', '<'))
-        if i % 1000 == 0: print(i)
-        myMap_2.map.append(c)
-        # find next obstacle until we hit the edge of the map or revisit
-        hitEdge, pVisited = False, False
-        while not hitEdge and not pVisited:
-            hitEdge = myMap_2.move()
-            pVisited = tuple(myMap_2.pos) in myMap_2.path
-            myMap_2.path.append(tuple(myMap_2.pos))
-        result += 1 if pVisited else 0
-        pass
-    print(i)
-
-    message = f'The answer to part 2 is (sample should be 6, answer should be 1995): {result}\n'
+    result = sum([r[0] for r in myCal if True in r[3]])
+    message = f'The answer to part 1 is (sample should be 3749, answer should be ?): {result}\n'
     print(message)
     pass
 
@@ -119,7 +61,7 @@ parser.add_argument("-v", "--verbose"   , action="store_true", default=False, he
 parser.add_argument("-p", "--production", action="store_true", default=False, help="Add this to try the puzzle input")
 args = parser.parse_args()
 
-day = '06'
+day = '07'
 fName = f'2024/input/{day}_sample.txt'
 if args.production: fName = f'2024/input/{day}.txt'
 
